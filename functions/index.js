2beeae8ec4d98669cdfdfa5ed8e76ac674f1e9fe
Dymas <dymassousa@gmail.com>
2026-04-14 23:47:30 -0300

    refactor: remove payment method necessity if free

--- functions/index.js
+++ functions/index.js
@@ -101,6 +101,7 @@ exports.createCheckoutSession = onRequest(
       const session = await stripe.checkout.sessions.create({
         customer: customer.id,
         payment_method_types: ["card"],
+        payment_method_collection: "if_required",
         line_items: [
           {
             price: priceId,
